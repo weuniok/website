c425a5d53065b78a9f6845e866cc6b63a0ff414e
weuniok <52104646+weuniok@users.noreply.github.com>
2023-04-05 19:57:11 +0200

    Initial commit

---
+++ src/build-time/index.ts
@@ -0,0 +1,46 @@
+import { resolve } from "node:path";
+import shikiTwoslash from "remark-shiki-twoslash";
+import remarkSupersub from "remark-supersub";
+import type { Pluggable, PluggableList } from "unified";
+
+import { titleCase } from "../lib/titleCase";
+
+import { asidesPlugin } from "./asidesPlugin";
+import { derivedTitleAndDatePlugin } from "./derivedTitleAndDatePlugin";
+import {
+  readingTimePlugin,
+  remarkMdxReadingTimePlugin,
+} from "./readingTimePlugin";
+import { urlOutsideOfPagesDirPlugin } from "./urlOutsideOfPagesDirPlugin";
+
+export const remarkPlugins = (projectDir: string): PluggableList => {
+  return checkOptions(
+    [
+      urlOutsideOfPagesDirPlugin,
+      { absoluteDirPath: resolve(projectDir, "./posts") },
+    ],
+    [derivedTitleAndDatePlugin, { title: titleCase }],
+    remarkSupersub,
+    [shikiTwoslash, { themes: ["github-light", "github-dark"] }],
+    readingTimePlugin,
+    remarkMdxReadingTimePlugin
+  );
+};
+
+export const rehypePlugins: PluggableList = checkOptions([asidesPlugin, {}]);
+
+/**
+ * Adds autocomplete and typechecking to plugin tuples.
+ */
+function checkOptions<TPlugins extends Pluggable[]>(
+  ...p: {
+    [I in keyof TPlugins]: TPlugins[I] extends [
+      infer TPlugin extends (...args: never[]) => unknown,
+      unknown
+    ]
+      ? [TPlugin, ...Parameters<TPlugin>]
+      : TPlugins[I];
+  }
+) {
+  return p;
+}
---
+++ src/lib/Link.tsx
@@ -0,0 +1,44 @@
+import { JSX, splitProps } from "solid-js";
+
+export interface LinkProps extends JSX.AnchorHTMLAttributes<HTMLAnchorElement> {
+  noUnderline?: boolean;
+}
+
+export function Link(props: LinkProps) {
+  const [own, rest] = splitProps(props, ["classList", "noUnderline"]);
+
+  const childIsImg = isChildType(rest.children, "img");
+
+  return (
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
+    <a
+      classList={{
+        ...own.classList,
+        [" underline underline-offset-4 decoration-gray-200 dark:decoration-gray-700" +
+        " hover:decoration-transparent dark:hover:decoration-transparent" +
+        " focus:decoration-transparent dark:focus:decoration-transparent"]:
+          !own.noUnderline,
+        "p-2 -mx-2 rounded-sm before:rounded-sm transition-colors relative":
+          true,
+        "zaduma-hover-before": true,
+        "zaduma-image-box": childIsImg,
+      }}
+      {...rest}
+    />
+  );
+}
+
+function isChildType(children: JSX.Element, type: string) {
+  if (!children || typeof children !== "object") return false;
+
+  // A child can be a JSX element or an stringified Astro slot.
+  if ("type" in children) return children.type === type;
+  if ("t" in children) {
+    return (
+      (children.t as string).startsWith(`<astro-slot><${type} `) ||
+      (children.t as string).startsWith(`<astro-slot><span zaduma-image`)
+    );
+  }
+
+  return false;
+}
